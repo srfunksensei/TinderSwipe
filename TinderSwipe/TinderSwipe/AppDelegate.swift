48fc056049803a945a8b58055086aef018df522a
srfunksensei <sr.funk.sensei@gmail.com>
2020-09-11 11:05:19 +0200

    initial commit

---
+++ TinderSwipe/TinderSwipe/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TinderSwipe
+//
+//  Created by MB on 11/09/2020.
+//  Copyright © 2020 MB. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
